28d37b0f4b466d2aa2c50528cfa6914efe72a5c2
MATHEUS <cg3014967@LAB117M09.IFSP>
2022-05-24 16:07:23 -0300

    arrumando

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ define('BASE_URL_API','/lp2/api/');
 
 //database config
 define('DB_HOST','localhost');
-define('DB_NAME','estudosapi');
+define('DB_NAME','estudoapi');
 define('DB_USER_NAME','root');
 define('DB_PASSWORD','');
 
